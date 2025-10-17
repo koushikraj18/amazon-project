03f83953ef8a5942849f707bef1909be39620c34
KoushikRaj <kantekoushikraj18@gmail.com>
2025-10-17 16:41:52 +0530

    load products with backend

--- data/products.js
+++ data/products.js
@@ -56,7 +56,29 @@ class Clothing extends Product{
   }
 }
 
-export const products = [
+export let products = [];
+
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('load',() => {
+  products = JSON.parse(xhr.response).map((productDetails) => {
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
+   return new Product(productDetails);
+});
+
+console.log(products);
+fun();
+
+});
+
+xhr.open('GET','https://supersimplebackend.dev/products');
+xhr.send();
+}
+
+/*export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -721,3 +743,6 @@ export const products = [
   }
    return new Product(productDetails);
 });
+*/
+//const date = Date();
+//console.log(date);
