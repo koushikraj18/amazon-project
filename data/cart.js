90518bc83d039756b4942c71a24797b3158cd654
KoushikRaj <kantekoushikraj18@gmail.com>
2025-10-22 10:36:04 +0530

    learn promise and convert code

--- data/cart.js
+++ data/cart.js
@@ -69,3 +69,18 @@ export function addTOCart(productId){
 
     saveToStorage();
   }
+
+  export function loadCart(fun){
+    
+  const xhr = new XMLHttpRequest();
+  
+  xhr.addEventListener('load',() => {
+
+  console.log(xhr.response);
+  fun();
+  
+  });
+  
+  xhr.open('GET','https://supersimplebackend.dev/cart');
+  xhr.send();
+  }
