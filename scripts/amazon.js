ab29b9b0732fe31359fcc84fd23f4748838e43cc
koushikraj18 <147997446+koushikraj18@users.noreply.github.com>
2025-12-16 18:19:00 +0530

    Change redirect URL from amazon.html to index.html

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -111,7 +111,7 @@ async function renderProducts() {
     .addEventListener('click', () => {
 
       const search = document.querySelector('.js-search-bar').value;
-      window.location.href = `amazon.html?search=${search}`;
+      window.location.href = `index.html?search=${search}`;
     });
     
     document.querySelector('.js-search-bar')
