64af8d14290bafe4630513b6a79f5c20c8a55acb
koushikraj18 <147997446+koushikraj18@users.noreply.github.com>
2025-12-16 14:31:51 +0530

    Change redirect URL from amazon.html to index.html

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -120,7 +120,7 @@ async function renderProducts() {
       if (event.key === 'Enter') {
 
         const searchTerm = document.querySelector('.js-search-bar').value;
-        window.location.href = `amazon.html?search=${searchTerm}`;
+        window.location.href = `index.html?search=${searchTerm}`;
       }
     });
   }
